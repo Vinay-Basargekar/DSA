b54b3fa7be62f9fefb0b90b405145932670dde1a
Mona Lisa <vinaybasargekar@gmail.com>
2024-01-13 11:03:54 +0530

    feat: binarySearch code

--- Searching/Binary Search/Binary_with_function.cpp
+++ Searching/Binary Search/Binary_with_function.cpp
@@ -1,35 +1,33 @@
 #include <iostream>
 using namespace std;
 
-int dhundo(int even[], int size, int key)
+int binarySearch(int arr[], int size, int key)
 {
-    int start = 0;
-    int end = size - 1;
-    int mid = start + (end - start) / 2;
+    int low = 0, high = size - 1;
 
-    while (start <= end)
+    while (low <= high)
     {
+        int mid = (low + high) / 2;
 
-        if (even[mid] == key)
-        {
-            return mid;
-        }
-        if (key > even[mid])
-        {
-            start = mid + 1;
-        }
-        if (key < even[mid])
-        {
-            end = mid - 1;
-        }
-        mid = start + (end - start) / 2;
+        if (arr[mid] == key)
+            return 1;
+        else if (key >= arr[mid])
+            low = mid + 1;
+        else
+            high = mid - 1;
     }
     return -1;
 }
 
 int main()
 {
-    int even[] = {1, 2, 3, 4, 5, 6, 7, 8};
-    cout << dhundo(even, 8, 8);
-    return 0;
+    int arr[] = {1, 2, 3, 4, 5};
+    int key, c;
+    cout << "enter the key: ";
+    cin >> key;
+    c = binarySearch(arr, 5, key);
+    if (c == 1)
+        cout << "Found!";
+    else
+        cout << "Not found";
 }
