40416aa74aaa14eba5a8f09e80cc315a4f96cda6
Mona Lisa <vinaybasargekar@gmail.com>
2024-01-14 23:05:34 +0530

    feat : circular queue

--- queue/circular_Q.cpp
+++ queue/circular_Q.cpp
@@ -1,93 +1,132 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-const int SIZE=5;
+const int SIZE = 3;
 
-class CQueue{
-    int arr[SIZE];
-    int r,f,count;
-    public:
-    CQueue(){
-        f = 0; r = -1; count = 0;
+class CQueue
+{
+    int arr[SIZE], rear, front, count;
+
+public:
+    CQueue()
+    {
+        rear = -1;
+        front = 0;
+        count = 0;
     }
-    void insert(int x);
-    int delitem();
-    void dispq();
+
+    void insert(int x)
+    {
+        if (count == SIZE)
+        {
+            cout << "Full queue" << endl;
+        }
+        else
+        {
+            if (rear == SIZE - 1)
+            {
+                rear = -1;
+            }
+            arr[++rear] = x;
+            count++;
+        }
+    }
+
+    int delitem()
+    {
+        if (count == 0)
+        {
+            cout << "Queue is Empty" << endl;
+            return -1;
+        }
+        if (front == SIZE)
+        {
+            front = 0;
+        }
+        int y = arr[front++];
+        count--;
+        return y;
+    }
+
+    void display()
+    {
+        if (count == 0)
+        {
+            cout << "Queue is Empty" << endl;
+        }
+        else
+        {
+            int i = front;
+            int n = count;
+            while (n > 0)
+            {
+                if (i == SIZE)
+                {
+                    i = 0;
+                }
+                cout << arr[i] << " ";
+                i++;
+                n--;
+            }
+            cout << endl;
+        }
+    }
+
+    int isempty()
+    {
+        return (count == 0) ? 1 : 0;
+    }
 };
 
-void CQueue::insert(int x){
-    if(count == SIZE){
-        cout <<"Q full";
-        exit(0);
-    }
-    // if(r==SIZE-1){
-    //     r = 0;
-    // }
-    // arr[++r]=x;
-    // count++;
-    if (r == SIZE - 1){
-        r = 0; // Wrap around to the beginning of the array
-    }
-    else{
-        r++;
-    }
-    arr[r] = x;
-    count++;
-}
-
-int CQueue::delitem(){
-    if(count == 0){
-        cout <<"Q Empty";
-        exit(0);
-    }
-    int y;
-    if(f==SIZE){
-        f=0;
-    }
-    y=arr[f++];
-    count--;
-    return y;
-}
-
-// void CQueue::dispq()
-// {
-//     for (int i = f; i <= r; i++)
-//     {
-//         cout << arr[i] << " ";
-//     }
-//     cout<<endl;
-//     cout << count;
-// }
-
-void CQueue::dispq()
+int main()
 {
-    if (count == 0)
-    {
-        cout << "Queue is empty" << endl;
-        return;
-    }
-
-    int i = f;
-    do
-    {
-        cout << arr[i] << " ";
-        i = (i + 1) % SIZE;
-    } while (i != (r + 1) % SIZE); // Loop through the circular queue
-
-    cout << endl;
-    cout << "Count: " << count << endl;
-}
-
-int main(){
     CQueue q;
-    q.insert(1);
-    q.insert(2);
-    q.insert(3);
-    q.insert(4);
-    q.insert(4);
-    q.delitem();
-    q.delitem();
-    q.insert(4);
-    q.insert(4);
-    q.dispq();
+    int ch, data;
+    while (true)
+    {
+        cout << "\n---------------------";
+        cout << "\nQueue operations";
+        cout << "\n---------------------";
+        cout << "\n1. Insert an element";
+        cout << "\n2. Delete an element";
+        cout << "\n3. Display the queue";
+        cout << "\n4. Exit";
+        cout << "\nEnter your choice: ";
+        cin >> ch;
+
+        switch (ch)
+        {
+        case 1:
+            cout << "Enter the element to insert: ";
+            cin >> data;
+            q.insert(data);
+            break;
+
+        case 2:
+            if (!q.isempty())
+            {
+                int deletedItem = q.delitem();
+                cout << "Deleted item: " << deletedItem << endl;
+            }
+            else
+            {
+                cout << "Queue is empty. Cannot delete." << endl;
+            }
+            break;
+
+        case 3:
+            cout << "Current Queue: ";
+            q.display();
+            cout << endl;
+            break;
+
+        case 4:
+            cout << "\nExiting...";
+            exit(0);
+
+        default:
+            cout << "\nInvalid Choice" << endl;
+        }
+    }
+    return 0;
 }
