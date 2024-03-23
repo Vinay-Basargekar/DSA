b421199c72447f343265378c0ec9101d8eea4c28
Guffy-san <vinaybasargekar13@gmail.com>
2024-03-23 20:31:37 +0530

    pattern problems

--- pattern-printing/pattern5.cpp
+++ pattern-printing/pattern5.cpp
@@ -6,15 +6,15 @@
 #include<iostream>
 using namespace std;
 
-void pattern3(int n){
-    for(int i=1 ; i<=n ; i++){
-        for(int j=1 ; j<=i ; j++){
-            cout << j;
+void pattern5(int n){
+    for(int i=0 ; i<n ; i++){
+        for(int j=0 ; j<n-i ; j++){
+            cout << "* ";
         }
         cout << endl;
     }
 }
 
 int main(){
-    pattern3(4);
+    pattern5(3);
 }
---
+++ pattern-printing/pattern6.cpp
@@ -0,0 +1,20 @@
+//Pattern 6
+// 1 2 3
+// 1 2
+// 1
+
+#include<iostream>
+using namespace std;
+
+void pattern6(int n){
+    for(int i=1 ; i<=n ; i++){
+        for(int j=1 ; j<=n-i+1 ; j++){
+            cout << " " << j;
+        }
+        cout << endl;
+    }
+}
+
+int main(){
+    pattern6(3);
+}
