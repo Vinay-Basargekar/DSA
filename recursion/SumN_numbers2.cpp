9344ea65efb5f4e2d3d71f7441a506b5af21e775
Guffy-san <vinaybasargekar13@gmail.com>
2024-03-27 19:01:46 +0530

    sum of n number using functional recursion

---
+++ recursion/SumN_numbers2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int printSum(int n){
+    if(n==0){
+        return 0;
+    }
+    return n + printSum(n-1); 
+}
+
+int main(){
+    int sum = printSum(5);
+    cout << "Sum of number: " << sum;
+}
